c4702958492fd8508a9fcf4292856fef8fb2bb36
Saad Monem <smonem@umich.edu>
2024-12-06 13:44:36 -0500

    Added primary crime dataset

--- library.py
+++ library.py
@@ -1,6 +1,15 @@
 # We can include functions and methods here...
 # They could be resuable for other dataset, might be useful...
 
+import os
+import pandas as pd
 
-def func():
-    pass
+path_to_crime_csv = os.path.join("Datasets_Crime", "state_crime.csv")
+
+
+# To-Do: Can handle any necessary pre-processing here
+# or add arguments to return only crime rates or only counts, if useful
+def load_crime_df():
+    df = pd.read_csv(path_to_crime_csv)
+
+    return df
