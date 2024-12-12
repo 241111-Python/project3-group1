a22c2fd6cb2e08696be90e32028fe45ad5f4fd59
VitEfimov <135293952+VitEfimov@users.noreply.github.com>
2024-12-12 14:47:56 -0500

    split run and library

--- library.py
+++ library.py
@@ -5,17 +5,15 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-path_to_crime_csv = os.path.join("Datasets_Crime", "state_crime.csv")
 
+
+path_to_crime_csv = os.path.join("Datasets_Crime", "state_crime.csv")
 
 # To-Do: Can handle any necessary pre-processing here
 # or add arguments to return only crime rates or only counts, if useful
 def load_crime_df():
     df = pd.read_csv(path_to_crime_csv)
-
     return df
-
-# date_patterns = re.compile(r'(date|year)', re.IGNORECASE)
 
 def change_column_header(colomns,date_patterns):
     changed = []
@@ -29,19 +27,15 @@ def change_column_header(colomns,date_patterns):
 
 # convert date to year format
 def date_to_year_format(df, date_column):
-    # df[date_column] = pd.to_datetime(df[date_column], format='%Y', errors='coerce')
-    
-
     df[date_column] = pd.to_datetime(df[date_column], errors='coerce')
-
     df[date_column] = df[date_column].dt.year
     return df
 
 # check if 'Year/Date' column  convert it to year format
 def check_format_date_column(df):
-    df['Year/Date'] = df['Year/Date'].astype(str)
-    if 'Year/Date' in df.columns:
-        df = date_to_year_format(df, 'Year/Date')
+    df["Year/Date"] = df["Year/Date"].astype(str)
+    if "Year/Date" in df.columns:
+        df = date_to_year_format(df, "Year/Date")
     return df
 # calculate average, grouped by group_col='Year/Date' in call, can be used for other
 def count_string_data(df, group_col, value_col, string):
@@ -55,15 +49,21 @@ def count_string_data(df, group_col, value_col, string):
 
 # calculate average, grouped by 'Year/Date'
 def calculate_avg(df, attribute):
+    if df[attribute].dtype == "object":
+        df[attribute] = df[attribute].str.replace(',', '').astype(int)
     return df.groupby('Year/Date')[attribute].mean().reset_index()
 
 # calculate sum, grouped by 'Year/Date'
 def calculate_sum(df, attribute):
-    return df.groupby('Year/Date')[attribute].sum().reset_index()
+    if df[attribute].dtype == "object":
+        df[attribute] = df[attribute].str.replace(',', '').astype(int)
+    return df.groupby("Year/Date")[attribute].sum().reset_index()
 
 # calculate count, grouped by 'Year/Date'
 def calculate_count(df, attribute):
-    return df.groupby('Year/Date')[attribute].count().reset_index()
+    if df[attribute].dtype == "object" or pd.api.types.is_string_dtype(df[attribute]):
+       df[attribute] = df[attribute].str.replace(',', '').astype(int)
+    return df.groupby("Year/Date")[attribute].count().reset_index()
 
 # conclusion correlation between two attributes
 def corr_conclusions(correlation_coefficient):
@@ -84,19 +84,19 @@ def corr_conclusions(correlation_coefficient):
 
 # Plotting
 def plotting(merged_df, attr1, attr2, color1, color2, correlation_coefficient):
-    years = merged_df['Year/Date']
+    years = merged_df["Year/Date"]
     attribute1 = merged_df[attr1]
     attribute2 = merged_df[attr2]
     fig, ax1 = plt.subplots(figsize=(12, 6))
-    ax1.set_xlabel('Year')
+    ax1.set_xlabel("Year")
     ax1.set_ylabel(attr1, color=color1)
-    ax1.plot(years, attribute1, label=attr1, color=color1, marker='o', linestyle='-')
-    ax1.tick_params(axis='y', labelcolor=color1)
+    ax1.plot(years, attribute1, label=attr1, color=color1, marker="o", linestyle="-")
+    ax1.tick_params(axis="y", labelcolor=color1)
     ax2 = ax1.twinx()
     ax2.set_ylabel(attr2, color=color2)
-    ax2.plot(years, attribute2, label=attr2, color=color2, marker='o', linestyle='-')
-    ax2.tick_params(axis='y', labelcolor=color2)
-    fig.suptitle(f'{attr1} vs. {attr2}\n{corr_conclusions(correlation_coefficient)}')
+    ax2.plot(years, attribute2, label=attr2, color=color2, marker="o", linestyle="-")
+    ax2.tick_params(axis="y", labelcolor=color2)
+    fig.suptitle(f'{attr1} vs. {attr2}\n{corr_conclusions(correlation_coefficient)}\n{correlation_coefficient:2f}')
     # plt.savefig(f'{attr1}_vs_{attr2}.png')
     output_dir = 'correlation-graphs'
     if not os.path.exists(output_dir):
---
+++ run.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import re
+import matplotlib.pyplot as plt
+import os
+from library import *
+
+#Get numeric choice
+def get_choice(options, prompt):
+    print(prompt)
+    for i, option in enumerate(options, start=1):
+        print(f"{i}. {option}")
+    while True:
+        try:
+            choice = int(input("Enter the number of your choice: "))
+            if 1 <= choice <= len(options):
+                return options[choice - 1]
+            else:
+                print("Invalid choice. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+#re for find pattern
+date_pattern = re.compile(r'(date|year)', re.IGNORECASE)
+
+#Enter to the program
+print("Data correlation tool!")
+
+# Load data
+path1 = input("Enter the path to dataset 1: ")
+path2 = input("Enter the path to dataset 2: ")
+# df1 = pd.read_csv(path1)
+# df2 = pd.read_csv(path2)
+df1 = pd.read_csv(path1, encoding="ISO-8859-1")
+df2 = pd.read_csv(path2, encoding="ISO-8859-1")
+
+# Rename columns
+df1.columns = change_column_header(df1.columns,date_pattern)
+df2.columns = change_column_header(df2.columns,date_pattern)
+
+# Check date and format to YYYY
+df1 = check_format_date_column(df1)
+df2 = check_format_date_column(df2)
+print(df1.head())
+print(df2.head())
+
+#Choose the column Dataset 1
+print("\nChoose the column to analyze in Dataset 1:")
+attribute1 = get_choice(df1.columns, "Columns in Dataset 1:")
+print("attribute1",attribute1)
+
+#Choose the column Dataset 2
+print("\nChoose the column to analyze in Dataset 2:")
+attribute2 = get_choice(df2.columns, "Columns in Dataset 2:")
+print("attribute2",attribute2)
+
+# assign value if string data in colomn
+value1 = attribute1
+value2 = attribute2
+
+# operations
+methods = ["Count string data", "Average for numerical", "Sum for numerical", "Count for numerical"]
+operation = get_choice(methods, "Choose an operation dataset1:")
+df1_appl = None
+
+# Apply function to df1
+if operation == "Count string data":
+    value1 = input(f"Enter the string to count {attribute1}: ")
+    df1_appl = count_string_data(df1, "Year/Date", attribute1, value1)
+elif operation == "Average for numerical":
+    df1_appl = calculate_avg(df1, attribute1)
+elif operation == "Sum for numerical":
+    df1_appl = calculate_sum(df1, attribute1)
+elif operation == "Count for numerical":
+    df1_appl = calculate_count(df1, attribute1)
+
+print(df1_appl)
+
+# Apply function to df2
+operation = get_choice(methods, "Choose an operation dataset2:")
+df2_appl = None
+if operation == "Count string data":
+    value2 = input(f"Enter the string to count {attribute2}: ")
+    df2_appl = count_string_data(df2, "Year/Date", attribute2, value2)
+elif operation == "Average for numerical":
+    df2_appl = calculate_avg(df2, attribute2)
+elif operation == "Sum for numerical":
+    df2_appl = calculate_sum(df2, attribute2)
+elif operation == "Count for numerical":
+    df2_appl = calculate_count(df2, attribute2)
+
+print(df2_appl)
+
+#merge dataframes
+merged_df = pd.merge(df1_appl, df2_appl, on="Year/Date", how="outer")
+print(merged_df)
+
+#if by applying string value to change the column name and iteract in merged_df
+attribute1 = value1
+attribute2 = value2
+
+merged_df.fillna(0, inplace=True)
+
+print("\nMerged DataFrame:")
+print(merged_df)
+
+# filtering data to get specific data frame
+min_year_df1 = merged_df[merged_df[attribute1] != 0]["Year/Date"].min()
+max_year_df1 = merged_df[merged_df[attribute1] != 0]['Year/Date'].max()
+min_year_df2 = merged_df[merged_df[attribute2] != 0]["Year/Date"].min()
+max_year_df2 = merged_df[merged_df[attribute2] != 0]["Year/Date"].max()
+
+#definition min and max year in df
+min_year = max(min_year_df1,min_year_df2)
+max_year = max(max_year_df1,max_year_df2)
+
+#filter
+filtered_df = merged_df[(merged_df["Year/Date"] >= min_year) & (merged_df["Year/Date"] <= max_year)]
+
+print(f"\nFiltered DataFrame (Years {min_year}-{max_year}):")
+print(filtered_df)
+
+# correlation
+correlation_coefficient = filtered_df[attribute1].corr(filtered_df[attribute2])
+
+# plotting
+print(f"\nCorrelation coefficient: {correlation_coefficient}")
+print(corr_conclusions(correlation_coefficient))
+plotting(filtered_df, attribute1, attribute2, "blue", "red", correlation_coefficient)
